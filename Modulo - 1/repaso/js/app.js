dcc969a31d6e13badddab0187b697572c078438b
Eraldo Vicente <eraldovicente2@gmail.com>
2020-06-29 23:17:01 -0300

    spread operator

--- Modulo - 1/repaso/js/app.js
+++ Modulo - 1/repaso/js/app.js
@@ -262,3 +262,31 @@ const {nombre} = persona;
 console.log(nombre);
 
 console.log(Object.keys(persona));
+
+
+// Sprear operator
+
+let lenguajes = ['JavaScript', 'PHP', 'Python'];
+let frameworks = ['ReactJS', 'Laravel', 'Django'];
+
+// unir los arreglosen 1 solo
+
+// let combinacion = lenguajes.concat(frameworks);
+// let combinacion = [...lenguajes,...frameworks];
+// let nuevoArreglo = [...lenguajes];
+
+// console.log(nuevoArreglo);
+
+let [ultimo] = [...lenguajes].reverse();
+
+console.log(lenguajes);
+console.log(ultimo);
+
+
+function suma(a, b, c) {
+     console.log(a+b+c);
+}
+
+const numeros = [1,2,3];
+
+suma(...numeros);

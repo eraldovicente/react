e9a5b688966a125438b249bd08d84f0f9a2ee28b
Eraldo Vicente <eraldovicente2@gmail.com>
2020-07-01 21:05:03 -0300

    basico de JSX

--- Modulo - 2/basicos/src/components/Header.jsx
+++ Modulo - 2/basicos/src/components/Header.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 function Header() {
 
+     const edad = 18;
+
+     let mensaje;
+     if (edad >= 18) {
+          mensaje = 'Eres mayor de edad';
+     } else {
+          mensaje = 'Eres menor de edad';
+     }
+
      return(
-          <h1>Desde el header</h1>
+          <>
+               <h1>{mensaje}</h1>
+               <h1 id="encabezado" className="encabezado">Tienda Virtual</h1>
+          </>
      )
 }
 

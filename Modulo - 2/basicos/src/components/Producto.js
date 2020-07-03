fa88a3677b39cde609bcda4996aa27cb1c350711
Eraldo Vicente <eraldovicente2@gmail.com>
2020-07-03 17:53:11 -0300

    Eventos en React

--- Modulo - 2/basicos/src/components/Producto.js
+++ Modulo - 2/basicos/src/components/Producto.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 const Producto = ({producto}) => {
 
+     // Agregar producto al carrito
+     const seleccionarProducto = id => {
+          console.log('Comprando... ', id);
+     }
 
      const { nombre, precio, id } = producto;
      return ( 
           <div>
                <h2>{nombre}</h2>
                <p>${precio}</p>
+               <button 
+                    type="button"
+                    onClick={ () => seleccionarProducto(id) }
+               >Comprar</button>
           </div>
      );
 }

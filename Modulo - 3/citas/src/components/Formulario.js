d2227beed7181af5056f0fc8feee12941f5b251e
Eraldo Vicente <eraldovicente2@gmail.com>
2020-07-04 20:55:44 -0300

    Leyendo lo que lo usuario escribe

--- Modulo - 3/citas/src/components/Formulario.js
+++ Modulo - 3/citas/src/components/Formulario.js
@@ -3,7 +3,7 @@ import React, { Fragment, useState } from 'react';
 const Formulario = () => {
 
      // Crear State de Citas
-     const [citas, actualizarCita] = useState({
+     const [cita, actualizarCita] = useState({
           mascota: '',
           propietario: '',
           fecha: '',
@@ -12,9 +12,15 @@ const Formulario = () => {
      });
 
      // Funcion que se ejecuta cada que el usuario escribe en un input
-     const actualizarState = () => {
-          console.log('escribiendo...');
+     const actualizarState = e => {
+          actualizarCita({
+               ...cita,
+               [e.target.name]: e.target.value
+          })
      }
+
+     // Extraer los valores
+     const { mascota, propietario, fecha, hora, sintomas } = cita;
 
      return ( 
           <Fragment>
@@ -28,6 +34,7 @@ const Formulario = () => {
                          className="u-full-width"
                          placeholder="Nombre Mascota"
                          onChange={actualizarState}
+                         value={mascota}
                     />
                     <label>Nombre Dueño</label>
                     <input
@@ -36,6 +43,7 @@ const Formulario = () => {
                          className="u-full-width"
                          placeholder="Nombre Dueño de la mascota"
                          onChange={actualizarState}
+                         value={propietario}
                     />
                     <label>Fecha</label>
                     <input
@@ -43,6 +51,7 @@ const Formulario = () => {
                          name="fecha"
                          className="u-full-width"
                          onChange={actualizarState}
+                         value={fecha}
                     />
                     <label>Hora</label>
                     <input
@@ -50,12 +59,14 @@ const Formulario = () => {
                          name="hora"
                          className="u-full-width"
                          onChange={actualizarState}
+                         value={hora}
                     />
                     <label>Síntomas</label>
                     <textarea
                          className="u-full-width"
                          name="sintomas"
                          onChange={actualizarState}
+                         value={sintomas}
                     ></textarea>
                     <button
                          type="submit"

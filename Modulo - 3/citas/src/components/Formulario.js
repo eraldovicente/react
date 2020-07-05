1c932381f8f23f2b97afffe65e0fdda8af5c16e9
Eraldo Vicente <eraldovicente2@gmail.com>
2020-07-05 02:27:45 -0300

    Reiniciar el formulario

--- Modulo - 3/citas/src/components/Formulario.js
+++ Modulo - 3/citas/src/components/Formulario.js
@@ -44,6 +44,13 @@ const Formulario = ({crearCita}) => {
           crearCita(cita);
 
           // Reiniciar el form
+          actualizarCita({
+               mascota: '',
+               propietario: '',
+               fecha: '',
+               hora: '',
+               sintomas: ''    
+          })
 
      }
 

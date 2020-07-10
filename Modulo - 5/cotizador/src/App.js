342cb9d53299d95a8cc76e88d03ef4329dc31f7a
Eraldo Vicente <eraldovicente2@gmail.com>
2020-07-09 23:16:56 -0300

    Criando styled components

--- Modulo - 5/cotizador/src/App.js
+++ Modulo - 5/cotizador/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
 
+import styled from '@emotion/styled'; 
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
+
 function App() {
   return (
-    <Header 
-      titulo='Cotizador de Seguros'
-    />
+    <Contenedor>
+      <Header 
+        titulo='Cotizador de Seguros'
+      />
+    </Contenedor>
+
+    <ContenedorFormulario>
+      
+    </ContenedorFormulario>
   );
 }
 

648eec4730d05fb62ea5532eed02a7aac38e7d54
Eraldo Vicente <eraldovicente2@gmail.com>
2020-07-11 02:20:08 -0300

    Criando o componente de Resumo

--- Modulo - 5/cotizador/src/components/Formulario.js
+++ Modulo - 5/cotizador/src/components/Formulario.js
@@ -50,7 +50,7 @@ const Error = styled.div`
      margin-bottom: 2rem;
 `;
 
-const Formulario = () => {
+const Formulario = ({guardarResumen}) => {
 
      const [ datos, guardarDatos ] = useState({
           marca: '',
@@ -104,7 +104,10 @@ const Formulario = () => {
 
           console.log(resultado);
 
-          // Total
+          guardarResumen({
+               cotizacion: resultado,
+               datos
+          });
      }
 
      return ( 

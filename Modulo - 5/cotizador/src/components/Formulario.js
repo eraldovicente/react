61f4f7267c57858c5a1538b21d569072f6bf607b
Eraldo Vicente <eraldovicente2@gmail.com>
2020-07-11 01:34:47 -0300

    Calculando o custo com base na marca

--- Modulo - 5/cotizador/src/components/Formulario.js
+++ Modulo - 5/cotizador/src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { obtenerDiferenciaYear } from '../helper';
+import { obtenerDiferenciaYear, calcularMarca } from '../helper';
 
 const Campo = styled.div`
      display: flex;
@@ -92,11 +92,12 @@ const Formulario = () => {
           // por cada año hay que restar el 3%
           resultado -= (( diferencia * 3 ) * resultado ) / 100;
 
-          console.log(resultado);
-
           // Amaricano 15%
           // Asiatica 5%
           // Europeia 30%
+          resultado = calcularMarca(marca) * resultado;
+
+          console.log(resultado);
 
           // Basico aumenta 20%
           // Completo 50%

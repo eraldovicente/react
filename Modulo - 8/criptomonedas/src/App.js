bb90a8c798dc41e8b42e9ab755aa747da0775c9c
Eraldo Vicente <eraldovicente2@gmail.com>
2020-07-19 23:40:24 -0300

    Passando valores a cotiza a um Effect

--- Modulo - 8/criptomonedas/src/App.js
+++ Modulo - 8/criptomonedas/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import imagen from './cryptomonedas.png';
 import Formulario from './components/Formulario';
@@ -37,6 +37,19 @@ const Heading = styled.h1`
 `;
 
 function App() {
+
+  const [moneda, guardarMoneda] = useState('');
+  const [criptomoneda, guardarCriptomoneda] = useState('');
+
+  useEffect(() => {
+
+    // evitamos la ejecucion la primera vez
+    if (moneda === '') return;
+
+    console.log('Cotizando...');
+
+  }, [moneda, criptomoneda]);
+
   return (
     <Contenedor>
       <div>
@@ -47,7 +60,11 @@ function App() {
       </div>
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
-        <Formulario />
+
+        <Formulario 
+          guardarMoneda={guardarMoneda}
+          guardarCriptomoneda={guardarCriptomoneda}
+        />
       </div>
     </Contenedor>     
   );

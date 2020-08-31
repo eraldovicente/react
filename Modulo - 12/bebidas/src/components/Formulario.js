84d18c75c4ca3e2c7419f089cc5e8a147cd68de3
Eraldo Vicente <eraldovicente2@gmail.com>
2020-08-31 15:36:53 -0300

    Criando o formulario

--- Modulo - 12/bebidas/src/components/Formulario.js
+++ Modulo - 12/bebidas/src/components/Formulario.js
@@ -2,8 +2,43 @@ import React from 'react';
 
 const Formulario = () => {
      return ( 
-          <h1>Formulario aqui</h1>
-      );
+          <form
+               className="col-12"
+          >
+               <fieldset className="text-center">
+                    <legend>Buscar bebidas por Categoria o Ingrediente</legend>
+               </fieldset>
+
+               <div className="row">
+                    <div className="col-md-4">
+                         <input
+                              name="nombre"
+                              className="form-control"
+                              type="text"
+                              placeholder="Buscar por Ingrediente"
+                         />
+                    </div>
+                    <div className="col-md-4">
+                         <select
+                              className="form-control"
+                              name="categoria"
+                         >
+                              <option value="">-- Selecciona Categoria --</option>
+                         </select>
+                    </div>
+
+                    <div className="col-md-4">
+                         <input
+                              type="submit"
+                              className="btn btn-block btn-primary"
+                              value="Buscar Bebidas"
+                         >
+                         </input>
+                    </div>
+               </div>
+
+          </form>
+     );
 }
  
 export default Formulario;

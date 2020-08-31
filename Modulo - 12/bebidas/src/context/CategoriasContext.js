d501304da8cac1596c8cca3322d576f1cc953855
Eraldo Vicente <eraldovicente2@gmail.com>
2020-08-31 16:41:47 -0300

    Criando o Context

---
+++ Modulo - 12/bebidas/src/context/CategoriasContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from 'react';
+
+// Crear el Context
+export const CategoriasContext = createContext();
+
+// Provider es donde se encuentran las funciones y state
+const CategoriasProvider = (props) => {
+
+     // crear el state del Context
+     const [ hola, guardaHola ] = useState('hola desde state');
+
+
+     return (
+          <CategoriasContext.Provider
+               value={{
+                    hola
+               }}
+          >
+               {props.children}
+          </CategoriasContext.Provider>
+     )
+}
+export default CategoriasProvider;

d6c9e611d5a62be6e4dec175770ad70612a397aa
Eraldo Vicente <eraldovicente2@gmail.com>
2020-09-11 13:13:17 -0300

    State e funcoes para nuevo proyecto

--- Modulo - 13/MERNTasks/cliente/src/components/proyectos/NuevoProyecto.js
+++ Modulo - 13/MERNTasks/cliente/src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const NuevoProyecto = () => {
+
+     // State para Proyecto
+     const [proyecto, guardarProyecto] = useState({
+          nombre: ''
+     });
+
+     // Extrair nombre de proyecto
+     const { nombre } = proyecto;
+
+     // Lee los contenidos del input 
+     const onChangeProyecto = e => {
+          guardarProyecto({
+               ...proyecto,
+               [e.target.name] : e.target.value
+          })
+     }
+
+     // Cuando el usuario envia un proyecto
+     const onSubmitProyecto = e => {
+          e.preventDefault();
+
+          // Validar el proyecto
+
+          // Agregar al State
+
+          // Reiniciar el form
+     }
+
      return ( 
           <Fragment>
                <button
@@ -10,12 +38,15 @@ const NuevoProyecto = () => {
 
                <form
                     className="formulario-nuevo-proyecto"
+                    onSubmit={onSubmitProyecto}
                >
                     <input
                          type="text"
                          className="input-text"
                          placeholder="Nombre Proyecto"
                          name="nombre"
+                         value={nombre}
+                         onChange={onChangeProyecto}
                     />
 
                     <input

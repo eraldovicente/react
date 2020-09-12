e9677275e0c2c3d77c0271701772f35eab8e60db
Eraldo Vicente <eraldovicente2@gmail.com>
2020-09-12 15:54:35 -0300

    Eliminando tareas

--- Modulo - 13/MERNTasks/cliente/src/components/tareas/ListadoTareas.js
+++ Modulo - 13/MERNTasks/cliente/src/components/tareas/ListadoTareas.js
@@ -35,6 +35,7 @@ const ListadoTareas = () => {
                          ? (<li className="tarea"><p>No hay tareas</p></li>)
                          : tareasproyecto.map(tarea => (
                               <Tarea
+                                   key={tarea.id}
                                    tarea={tarea}
                               />
                          ))

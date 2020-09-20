4e774b75d51c67deffdbeec13525d0905ad0f9d7
Eraldo Vicente <eraldovicente2@gmail.com>
2020-09-20 00:10:32 -0300

    Atualizar projetos com PUT

--- Modulo - 13/MERNTasks/servidor/routes/proyectos.js
+++ Modulo - 13/MERNTasks/servidor/routes/proyectos.js
@@ -19,4 +19,13 @@ router.get('/',
      proyectoController.obtenerProyectos
 );
 
+// Actualizar proyecto via ID
+router.put(':id',
+     auth,
+     [
+          check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+     ],
+     proyectoController.aactualizarproyecto
+);
+
 module.exports = router;
